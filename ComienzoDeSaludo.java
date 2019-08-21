d9cc5d5cf78f60dda60b7c7ac35ab70363992320
AxelGarciaTello <gata21022000@hotmail.com>
2019-08-21 07:49:04 -0500

    Funcion principal de saludo

---
+++ ComienzoDeSaludo.java
@@ -0,0 +1,9 @@
+public class ComienzoDeSaludo {
+    
+    public static void main(String[] args){
+        Saludo saludo = new Saludo();
+        saludo.preguntarNombre();
+        saludo.saludar();
+    }
+    
+}
