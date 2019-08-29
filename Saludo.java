1fa136aceadaaf077df45c7961592f38b08c29f7
AxelGarciaTello <gata21022000@hotmail.com>
2019-08-29 09:47:30 -0500

    Creacion del mensaje

--- Saludo.java
+++ Saludo.java
@@ -1,18 +1,12 @@
-
-import java.util.Scanner;
-
 public class Saludo {
+    private String mensaje;
     
-    private Scanner teclado = new Scanner(System.in);
-    private String nombre;
-    
-    public void preguntarNombre(){
-        System.out.println("¿Cúal es tu nombre?");
-        nombre=teclado.nextLine();
+    public void crearMensaje(){
+        mensaje="Hola mundo.\nMiau.";
     }
     
-    public void saludar(){
-        System.out.println("Hola "+nombre+".\nHola mundo.");
+    public String darMensaje(){
+        return mensaje;
     }
     
 }
